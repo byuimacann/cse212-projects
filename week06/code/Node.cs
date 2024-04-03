9e26adbfd737d23b27b21a0f269cdb8cd1295e77
byuimacann <mac20051@byui.edu>
2024-04-03 01:21:18 -0700

    wk 6 code - problems 1 & 3 complete

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,7 @@
+using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.Marshalling;
+
 public class Node {
     public int Data { get; set; }
     public Node? Right { get; private set; }
@@ -7,7 +11,7 @@ public class Node {
         this.Data = data;
     }
 
-    public void Insert(int value) {
+    public void Insert(int value) {   
         if (value < Data) {
             // Insert to the left
             if (Left is null)
@@ -16,18 +20,25 @@ public class Node {
                 Left.Insert(value);
         }
         else {
+        if (value > Data) {    
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
                 Right.Insert(value);
+            }
         }
-    }
+    }   
+    
 
     public bool Contains(int value) {
         // TODO Start Problem 2
+        
         return false;
-    }
+    }  
+     
+
+    
 
     public int GetHeight() {
         // TODO Start Problem 4
